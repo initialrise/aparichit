47f8a16c162235752d2dd1352386fc8e7401a091
initialrise <you@example.com>
2023-06-12 13:44:54 +0545

    started working with api

--- toilet.php
+++ toilet.php
@@ -3,7 +3,6 @@ include("includes/requests.php");
 echo "<h3>Toilets</h3>";
 $ok = json_decode(postRequest("https://erp.kathmandu.gov.np/apilink/village-profile-by-data",'{"data_type":"23"}'),true);
  $toilets = $ok["data"];
-
  foreach($toilets as $toilet){
     echo $toilet["sp_pp_name"]."<br>";
     echo "Location: ".$toilet["sp_pp_location_latitude"]." ".$toilet["sp_pp_location_longitude"]."<br>";
